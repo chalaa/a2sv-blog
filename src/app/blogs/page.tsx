02200eefb7e29d7b232868270a3815fa2aca8658
chalaa <chalaolanigeleta@gmail.com>
2024-08-28 09:34:27 +0300

    fix build error

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,19 +1,33 @@
+"use client"
 import BlogCard from "@/components/BlogCard/BlogCard";
 import BlogHeader from "@/components/BlogHeader/BlogHeader";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import { useGetAllBlogsQuery } from "../../../Services/api";
 
 export default function Home() {
+  const { data: data, error, isLoading } = useGetAllBlogsQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading blogs</div>;
+
+  const datas = data?.slice(0,5)
   return (
     <div>
       <Header />
         <BlogHeader />
       <div className="px-80">
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
+        {
+          datas?.map((d)=>(
+            <BlogCard 
+            key = {d._id}
+            _id={d._id} 
+            title={d.title}
+            description={d.description}
+            skills={d.skills}
+            />
+          ))
+        }
 
       </div>
       <Footer />
